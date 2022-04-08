2617ca142d30eaf666b1d023d38f7eaefa765e5d
Joaquin <joaquin.garcia.jimenez@gmail.com>
2022-04-08 10:07:26 +0200

    generamos proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './componenetes/cabecera/cabecera.component';
+import { FiltrosComponent } from './componenetes/filtros/filtros.component';
+import { MostrarDatosComponent } from './componenetes/mostrar-datos/mostrar-datos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    FiltrosComponent,
+    MostrarDatosComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/componenetes/mostrar-datos/mostrar-datos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mostrar-datos',
+  templateUrl: './mostrar-datos.component.html',
+  styleUrls: ['./mostrar-datos.component.scss']
+})
+export class MostrarDatosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
